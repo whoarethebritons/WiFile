a944772dd022f92e554f3b7f88d7c8c52727519f
kaitryn <kaitrynshayne@gmail.com>
2014-10-05 23:43:34 -0700

    added-should send ip and port

--- app/src/main/java/com/example/wifile/NsdActivity.java
+++ app/src/main/java/com/example/wifile/NsdActivity.java
@@ -1,13 +1,44 @@
 package com.example.wifile;
+
 import android.app.Activity;
-import android.os.*;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.net.nsd.NsdServiceInfo;
+import android.net.nsd.NsdManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.ServerSocket;
 
 /**
- * Created by Kait on 9/28/2014.
+ * Created by Kait on 10/4/2014.
  */
 public class NsdActivity extends Activity {
 
-   NsdHelper wfNsdHelper;
+    NsdManager wfNsdManager;
+    NsdServiceInfo wfService;
+    NsdHelper wfHelper;
+    private String wfIP;
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        wfHelper = new NsdHelper();
+        findDeviceIP(this);
+    }
 
-}// end class NsdActivity
+    public void findDeviceIP(Context context) {
+        WifiManager wifiMan = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiMan.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        wfIP = String.format("%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff), (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
+    }
+
+    public String getwfIP() {
+        return wfIP;
+    }
+}
