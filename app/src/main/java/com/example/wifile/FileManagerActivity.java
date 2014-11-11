eda231b203b70ea63a401175121528dc67e003d3
Karen <karentran64@gmail.com>
2014-11-11 11:10:51 -0800

    prepare checkbox

--- app/src/main/java/com/example/wifile/FileManagerActivity.java
+++ app/src/main/java/com/example/wifile/FileManagerActivity.java
@@ -2,6 +2,7 @@ package com.example.wifile;
 
 import android.app.ActionBar;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -12,27 +13,30 @@ import android.view.View;
 import android.widget.Checkable;
 import android.widget.ListView;
 import android.os.Environment;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+import android.util.SparseBooleanArray;
 
 /**
+ * FileManagerActivity is responsible for retrieving the selected files from the checkboxes
+ * and storing those selected files within a TEXT FILE. MAKE A FOLDER PUT A FILE IN THERE!!!!!!!
  * Created by Eden on 9/22/2014.
  */
-public class FileManagerActivity extends ListActivity implements Checkable {
-
-    //path string
+public class FileManagerActivity extends ListActivity {
+    private CheckAdapter checkAdapter;
     private String mPath;
-
-
+    //Create the history file, where we will store the files to be added
+    // to the computer. The file will be stored on the the phone's sd card
+    String FILEHISTORY = "fileHistory";
+    String[] fileDirectory;
 
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
 
+       /*---------------------Get the SD card directory into a File List--------------------------------------*/
         //set mPath to be sdcard
         //Environment.getExternalStorageDirectory();
+        //gets all files in the SD card
         mPath = Environment.getExternalStorageDirectory().getPath();
 
         if (getIntent().hasExtra("mPath")) {
@@ -54,7 +58,7 @@ public class FileManagerActivity extends ListActivity implements Checkable {
         if (!mFile.canRead()) {
             setTitle(mPath + "(inaccessible)");
         }
-
+        //List all the files in the current directory
         File[] flist = mFile.listFiles();
 
         //tests if the array is empty
@@ -68,19 +72,88 @@ public class FileManagerActivity extends ListActivity implements Checkable {
                 }
             }
         }
-        //sorts the array list
+        //sorts the array list in alphabetical order
         Collections.sort(dirs);
+        Object[] storeDirs = dirs.toArray(); //Convert List to an object array
+        fileDirectory = Arrays.copyOf(storeDirs, storeDirs.length,String[].class);//Convert to type String
 
-        //adapts the array list to work in list view of android
+        /*-----------------------------------------------------------------------------------------------------------*/
         //ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_checked, android.R.id.text1, dirs);
-        setListAdapter((new ArrayAdapter(this, R.layout.activity_filelist, R.id.nameView, dirs)));
+       // setListAdapter((new ArrayAdapter(this, R.layout.activity_filelist, R.id.nameView, dirs)));
+
+        //Create checkListAdaptor
+        checkAdapter = new CheckAdapter(this, R.layout.activity_filelist, R.id.nameView,
+                fileDirectory);
+        setListAdapter(checkAdapter);
         getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        //sets the list activity to use the array adapter
-        //setListAdapter(adapter);
+        //(Context context, int resource, int textViewResourceID, T[] objects )
+        //(context, references a single textview, field ID references a textview in the larger layout resource,
+        // array of objects to store into arrayadapter)
+
+        //Show the ListView and add OnClickListeners
+        //final ListView listView = getListView();
+        //listView.setOnItemClickListener(this);
     }
+
+    //CheckAdapter is for each checkbox in the list
+    public class CheckAdapter extends ArrayAdapter<String> {
+        Context context;
+        int resource;
+        int textViewResourceID;
+        SparseBooleanArray checkStates;
+
+        public CheckAdapter(Context context, int resource, int textViewResourceID, String[] list) {
+            super(context, resource, textViewResourceID, list);
+            checkStates = new SparseBooleanArray(list.length);
+        }
+
+        //Need a method to display the checkboxes as checked?
+        //When a checkbox is checked, rowChecked()
+
+        public boolean isChecked(int position) {
+            return checkStates.get(position, false);
+        }
+
+        public void setChecked(int position, boolean checked) {
+            checkStates.put(position, checked);
+        }
+
+        public void toggle(int position, boolean checked) {
+            setChecked(position, !checked);
+        }
+    }
+    //DONE button listener will retrieve all checked files
+
+    public void finish()
+    {
+        //Collect all the files
+        //send all filenames to writeToFile()
+         for(int i =0; i < checkAdapter.checkStates.size();i++)
+         {
+             if(checkAdapter.checkStates.get(i)== true)
+                //get the name of the application
+                //and writeToFile()
+         }
+    }
+
+    //Writes to the internal file, FILEHISTORY
+    public void writeToFile(String filename) throws IOException
+    {
+        String string = "some filename";
+        FileOutputStream fos = openFileOutput(FILEHISTORY, Context.MODE_PRIVATE);
+        try {
+            fos.write(string.getBytes());
+        } catch(IOException e) {
+
+        }
+        fos.close();
+    }
+
+    //Shows the files within each folder
     public void onListItemClick(ListView l, View v, int position, long id) {
 
-      //goes into folder
+        //List itemChecked = new ArrayList();
+        //goes into folder
         //retrieves the file name at the position you poked
         String filename = (String) getListAdapter().getItem(position);
 
@@ -97,7 +170,6 @@ public class FileManagerActivity extends ListActivity implements Checkable {
         if(new File(filename).isDirectory()) {
             Intent parent = getIntent();
 
-
             //creates new intent with the path of new directory
             Intent openDir = new Intent(this, FileManagerActivity.class);
             //assigns the path to the new activity
@@ -108,35 +180,11 @@ public class FileManagerActivity extends ListActivity implements Checkable {
         else {
 
         }
+        // itemChecked.add(filename);
     }
 
     public Intent getSupportParentActivityIntent () {
         return getIntent();
     }
 
-    @Override
-    public void setChecked(boolean checked) {
-
-    }
-
-    @Override
-    public boolean isChecked() {
-        return false;
-    }
-
-    @Override
-    public void toggle() {
-
-    }
-
-
-
-
-    public File[] getSelectedFiles(List checkDirs) {
-        File[] checked = null;
-        for(int i = 0; i< checkDirs.size(); i++) {
-            checked[i] = (File) checkDirs.get(i);
-        }
-        return checked;
-    }
 }
